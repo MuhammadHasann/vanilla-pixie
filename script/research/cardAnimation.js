383b97e8d5e01924235ec2b216392aa04ed045b7
Muhammad Hasan <muhammadhas772@gmail.com>
2024-05-18 19:48:17 +0800

    research pixie

--- script/research/cardAnimation.js
+++ script/research/cardAnimation.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const cardsContainer = document.querySelector(".research-card");
   const cards = Array.from(document.querySelectorAll(".research-card .card"));
+  const demoVideoCardAnimation = document.querySelector(".demo-video");
+  const sourceVideoCardAnimation =
+    demoVideoCardAnimation.querySelector("source");
 
-  // Initialize an array with the initial order of the cards
   let cardOrder = cards.map((card) =>
     parseInt(card.getAttribute("data-index-card"))
   );
 
-  function updateCardPositions() {
+  const updateCardPositions = () => {
+    cards.forEach((card) => {
+      card.className = card.className
+        .split(" ")
+        .filter((c) => !c.startsWith("card-position-"))
+        .join(" ");
+    });
+
     cardOrder.forEach((index, position) => {
       const cardElement = cards.find(
         (card) => parseInt(card.getAttribute("data-index-card")) === index
       );
 
-      // Set the z-index and bottom position based on the new order
       cardElement.style.zIndex = cards.length - position;
       cardElement.style.bottom = `${20 * position}%`;
 
-      // Enable or disable the click event based on position
+      cardElement.classList.add(`card-position-${position}`);
+
       if (position === 0) {
         cardElement.classList.remove("card-hover");
         cardElement.classList.add("card-active");
@@ -26,26 +34,35 @@ document.addEventListener("DOMContentLoaded", function () {
         cardElement.classList.remove("card-active");
       }
     });
-  }
 
-  cards.forEach((card, index) => {
-    card.addEventListener("click", function () {
-      const clickedIndex = parseInt(this.getAttribute("data-index-card"));
+    const cardPositionUpdatedEvent = new Event("cardPositionUpdated");
+    document.dispatchEvent(cardPositionUpdatedEvent);
+  };
+
+  cards.forEach((card) => {
+    card.addEventListener("click", (e) => {
+      const clickedIndex = parseInt(e.target.getAttribute("data-index-card"));
+
+      sourceVideoCardAnimation.src = `public/research/${clickedIndex - 1}.mp4`;
+      demoVideoCardAnimation.load();
+      demoVideoCardAnimation.play();
+
+      gsap.from(".research-demo-video", {
+        y: 50,
+        filter: "blur(4px)",
+        opacity: 0,
+      });
 
       if (cardOrder[0] !== clickedIndex) {
-        // Find the index of the clicked card in the cardOrder array
         const clickedCardIndex = cardOrder.indexOf(clickedIndex);
 
-        // Move the clicked card to the first position
-        cardOrder.splice(clickedCardIndex, 1); // Remove the clicked card from its current position
-        cardOrder.unshift(clickedIndex); // Add the clicked card to the start of the array
+        cardOrder.splice(clickedCardIndex, 1);
+        cardOrder.unshift(clickedIndex);
 
-        // Update the positions of the cards
         updateCardPositions();
       }
     });
   });
 
-  // Initial position update
   updateCardPositions();
 });
--- script/research/enterResearch.js
+++ script/research/enterResearch.js
@@ -1,18 +1,75 @@
-const subHeadingResearch = document.querySelector(".research-text span");
-const headingResearch = document.querySelector(".research-text h1");
+document.addEventListener("DOMContentLoaded", function () {
+  const subHeadingResearch = document.querySelector(".research-text span");
+  const headingResearch = document.querySelector(".research-text h1");
+  const cardClickAnimation = document.querySelectorAll(".card");
+  const demoVideo = document.querySelector(".demo-video");
 
-gsap.registerPlugin(ScrollTrigger);
+  const handleCardAnimation = () => {
+    const cardPosition1 = document.querySelector(".card-position-1");
+    const cardPosition2 = document.querySelector(".card-position-2");
 
-let tlResearch = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".research-text",
-    start: "top 90%",
-    end: "bottom top",
-    scrub: true,
-  },
+    cardPosition1.classList.add("card-animation-1");
+    cardPosition2.classList.add("card-animation-2");
+
+    setTimeout(() => {
+      cardPosition1.classList.remove("card-animation-1");
+      cardPosition2.classList.remove("card-animation-2");
+    }, 2000);
+  };
+
+  let tlResearch;
+  let cardAnimationBounce = false;
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  tlResearch = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".research-text",
+      start: "top 90%",
+      end: "bottom top",
+      scrub: true,
+      onUpdate: (e) => {
+        if (e.progress >= 1 && !cardAnimationBounce) {
+          cardClickAnimation.forEach((element) => {
+            element.style.pointerEvents = "all";
+          });
+
+          demoVideo.play();
+          handleCardAnimation();
+          cardAnimationBounce = true;
+        } else if (e.progress < 1 && cardAnimationBounce) {
+          cardClickAnimation.forEach((element) => {
+            element.style.pointerEvents = "none";
+          });
+
+          demoVideo.pause();
+          cardAnimationBounce = false;
+        }
+      },
+    },
+  });
+
+  const updateGSAPAnimation = () => {
+    tlResearch.clear();
+    tlResearch.play();
+    tlResearch.pause();
+
+    tlResearch
+      .from(subHeadingResearch, { y: 100, opacity: 0, overwrite: true })
+      .from(headingResearch, { y: 50, opacity: 0, overwrite: true }, "-=0.2")
+      .from(".research_demo", { opacity: 0, overwrite: true }, "-=1")
+      .from(".card-position-2", { opacity: 0, x: -400, overwrite: true })
+      .from(".card-position-1", { opacity: 0, x: -400, overwrite: true })
+      .from(".card-position-0", { opacity: 0, x: -400, overwrite: true });
+
+    tlResearch.progress(1);
+  };
+
+  updateGSAPAnimation();
+
+  document.addEventListener("cardPositionUpdated", () => {
+    updateGSAPAnimation();
+
+    !cardAnimationBounce && handleCardAnimation();
+  });
 });
-
-tlResearch
-  .from(subHeadingResearch, { y: 100, opacity: 0 })
-  .from(headingResearch, { y: 50, opacity: 0 }, "-=0.2")
-  .from(".research_demo", { opacity: 0 }, "-=1");
