3c75c0618c5e7c37bf6920d4ec59aad11a97aec6
Muhammad Hasan <muhammadhas772@gmail.com>
2024-05-20 14:23:48 +0800

    fix svg animation

--- script/backline/backline.js
+++ script/backline/backline.js
@@ -7,18 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 });
 
-const svgLine = document.querySelector(".svg-line");
-const svgPath = svgLine.querySelector("path");
-
-gsap.registerPlugin(DrawSVGPlugin, ScrollTrigger);
-
-gsap.from(svgPath, {
-  opacity: 0,
-  drawSVG: 0,
-  duration: 1.5,
-  ease: "ease-in",
-});
-
 const svgLineTwo = document.querySelector(".svg-line-2");
 const svgRect = svgLineTwo.querySelector("rect");
 
